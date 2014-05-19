da2628ff70f309ec02eb6017beaf53c457f3e59d
gustavojuan <gustavojuan@gmail.com>
2014-05-19 11:03:44 +0200

    1ª Finalizacion

--- CPRadio.php
+++ CPRadio.php
@@ -7,17 +7,19 @@ function CPRadius($lat, $lon, $radius)
     $sql = 'SELECT distinct(poblacion) FROM poblacion  WHERE (3958*3.1415926*sqrt((latitud-'.$lat.')*(latitud-'.$lat.') + cos(latitud/57.29578)*cos('.$lat.'/57.29578)*(longitud-'.$lon.')*(longitud-'.$lon.'))/180) <= '.$radius.';';
     $result = mysqli_query($conn,$sql);
 
-    // get each result
+    // Obtengo cada resultado
     $cplist = array();
     while($row = mysqli_fetch_array($result))
     {
         array_push($cplist, $row['poblacion']);
     }
 
-    //Preparo el array para procesado posterios
-    $separado_por_comas_2 = implode("','", $cplist);
+    //Preparo el array para procesado posterior (OPCIONAL)
+    $separado_por_comas = implode("','", $cplist);
 
-    return $separado_por_comas_2;
+    return $separado_por_comas;
+
+
     mysqli_free_result($conn);
     mysqli_close($conn);
 
